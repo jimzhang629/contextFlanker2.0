1506e692fc1c79dee3ac610e1178f00b5839b968
Jim Zhang <jzhang7345@gmail.com>
2022-11-22 17:05:16 -0500

    finalized datalogging

--- js/dataLogging.js
+++ js/dataLogging.js
@@ -1,27 +1,19 @@
 // data logging for sections
 // instructions, break screens, etc
 function logSectionData(){
-  data.push([expStage, sectionType, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, sectionStart, sectionEnd, sectionEnd - sectionStart]);
+  data.push([expStage, sectionType, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, sectionStart, sectionEnd, sectionEnd - sectionStart]);
   console.log(data);
 }
 
-// function logAdditionalSingletonTask(){
-//   let distractorLocation = (distractionArr[trialCount - 1] == "d") ? distractorLocationArr[trialCount - 1] : NaN;
-
-//   data.push([taskName, sectionType, block, blockLetter, trialCount, blockTrialCount, stimOnset, respOnset, respTime, getAccuracy(acc), targetShape, nonTargetShape, targetLocationArr[trialCount - 1], lineDirectionArr[trialCount - 1], distractionArr[trialCount - 1], distractorLineDirection, distractorLocation, switchRepeatArr[trialCount - 1], sequenceTypeArr[trialCount - 1], sequenceKindArr[trialCount - 1], sequencePositionArr[trialCount - 1], NaN, NaN, NaN]);
-//   console.log(data)
-// }
-
 function logMainTask(){
   //blockLetter is undefined..
-  data.push([taskName, sectionType, block, blockOrder, blockOrder[block], trialCount, blockTrialCount, trialImg, repeat, stimOnset, respOnset, respTime, getAccuracy(acc), locArray[trialCount-1], conArray[trialCount-1], imageSet[trialCount-1].src], repeatLog)
+  data.push([sectionType, taskName, block, blockOrder, blockOrder[block], trialCount, blockTrialCount, trialImg, trialImg.con, trialImg.loc, repeat, stimOnset, respOnset, respTime, getAccuracy(acc), locArray[trialCount-1], conArray[trialCount-1], imageSet[trialCount-1].src, NaN, NaN, NaN]);
   console.log('block is: ' + block);
 }
 
 function logPracticeTask(){
   // let distractorLocation = (distractionArr[trialCount - 1] == "d") ? distractorLocationArr[trialCount - 1] : NaN;
-
-  data.push([taskName, sectionType, NaN, NaN, NaN, trialCount, NaN, trialImg, stimOnset, respOnset, respTime, getAccuracy(acc), locArray[trialCount-1], conArray[trialCount-1], imageSet[trialCount-1].src]);
+  data.push([sectionType, taskName, NaN, NaN, NaN, trialCount, NaN, trialImg, trialImg.con, trialImg.loc, NaN, stimOnset, respOnset, respTime, getAccuracy(acc), locArray[trialCount-1], conArray[trialCount-1], imageSet[trialCount-1].src, NaN, NaN, NaN]);
 }
 
 /** logExpArrays stores the conArray, locArray, and repeatArray (which is actually two arrays)
