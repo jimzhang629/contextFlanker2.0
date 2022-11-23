93f1c37719a85a11b91c90644d378558b65f2bf5
Jim Zhang <jzhang7345@gmail.com>
2022-11-23 13:41:31 -0500

    getting rid of more trialCount dependencies

--- js/dataLogging.js
+++ js/dataLogging.js
@@ -7,13 +7,13 @@ function logSectionData(){
 
 function logMainTask(){
   //blockLetter is undefined..
-  data.push([sectionType, taskName, block, blockOrder, blockOrder[block], trialCount, blockTrialCount, trialImg, trialImg.con, trialImg.loc, repeat, stimOnset, respOnset, respTime, getAccuracy(acc), locArray[trialCount-1], conArray[trialCount-1], imageSet[trialCount-1].src, NaN, NaN, NaN]);
+  data.push([sectionType, taskName, block, blockOrder, blockOrder[block], trialCount, blockTrialCount, trialImg, trialImg.img.src, trialImg.con, trialImg.loc, repeat, stimOnset, respOnset, respTime, getAccuracy(acc), locArray[trialCount-1], conArray[trialCount-1], NaN, NaN, NaN]);
   console.log('block is: ' + block);
 }
 
 function logPracticeTask(){
   // let distractorLocation = (distractionArr[trialCount - 1] == "d") ? distractorLocationArr[trialCount - 1] : NaN;
-  data.push([sectionType, taskName, NaN, NaN, NaN, trialCount, NaN, trialImg, trialImg.con, trialImg.loc, NaN, stimOnset, respOnset, respTime, getAccuracy(acc), locArray[trialCount-1], conArray[trialCount-1], imageSet[trialCount-1].src, NaN, NaN, NaN]);
+  data.push([sectionType, taskName, NaN, NaN, NaN, trialCount, NaN, trialImg, trialImg.img.src, trialImg.con, trialImg.loc, NaN, stimOnset, respOnset, respTime, getAccuracy(acc), locArray[trialCount-1], conArray[trialCount-1], NaN, NaN, NaN]);
 }
 
 /** logExpArrays stores the conArray, locArray, and repeatArray (which is actually two arrays)
